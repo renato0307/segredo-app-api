789cb015f69bced15609a5d3f9cc037aa49eb5f5
Renato Torres <renato.torres@outsystems.com>
2020-11-04 22:48:11 +0000

    Implemented create secret operation

--- src/handler.py
+++ src/handler.py
@@ -1,8 +1,19 @@
 """
+Lambda handler supporting the segredo-app-api
 """
+import datetime
+import logging
+import os
+import random
+import string
+
+import boto3
+from botocore.exceptions import ClientError
 
 RESOURCE = "/{proxy+}"
-PATH = "/path"
+PATH = "/secret"
+
+ENV_DYNAMO_DB_REGION = "DYNAMO_DB_REGION"
 
 
 def handler(event, context):
@@ -14,7 +25,6 @@ def handler(event, context):
         return _invalid_path(path)
 
     method = event.get("httpMethod")
-
     if method == "POST":
         return _create_secret(event)
 
@@ -36,9 +46,44 @@ def _invalid_method(method):
     return {"statusCode": 501, "body": f"Method not supported: {method}"}
 
 
+def _unexpected_error():
+    return {"statusCode": 500, "body": "Unexcepted error"}
+
+
 def _create_secret(event):
-    return {"statusCode": 200, "body": "Secret created"}
+    try:
+        dynamodb = _get_dynamodb_client()
+        table = dynamodb.Table("Secrets")
+
+        random_id = _get_random_alphanumeric_string()
+        table.put_item(
+            Item={
+                "SecretId": random_id,
+                "SecretContent": event.get("body"),
+                "TimeToExist": int(datetime.datetime.utcnow().timestamp()),
+            }
+        )
+
+        return {"statusCode": 200, "body": {"SecretId": random_id}}
+    except ClientError as e:
+        logging.error("error creating secret: %s" % e)
+        return _unexpected_error()
+
+
+def _get_dynamodb_client():
+    return boto3.resource("dynamodb", region_name=_get_region())
 
 
 def _get_secret(event):
     return {"statusCode": 200, "body": "Secret retrieved"}
+
+
+def _get_random_alphanumeric_string():
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = "".join((random.choice(letters_and_digits) for i in range(80)))
+
+    return result_str
+
+
+def _get_region():
+    return os.getenv(ENV_DYNAMO_DB_REGION)
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,4 +1,12 @@
+"""
+Tests for lambda handler supporting the segredo-app-api
+"""
+import os
+
+import boto3
 import handler
+import moto
+from botocore.exceptions import ClientError
 
 
 def test_missing_resource():
@@ -32,3 +40,67 @@ def test_delete_is_invalid_method():
     result = handler.handler(event, None)
 
     assert result.get("statusCode") == 501
+
+
+@moto.mock_dynamodb2
+def test_create_secret():
+
+    # arranje
+    region = "us-east-1"
+    os.environ[handler.ENV_DYNAMO_DB_REGION] = region
+
+    table_name = "Secrets"
+    dynamodb = boto3.resource("dynamodb", region)
+    table = dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {"AttributeName": "SecretId", "KeyType": "HASH"},
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "SecretId", "AttributeType": "S"},
+        ],
+    )
+
+    content = "SECRET CONTENT"
+    event = {
+        "resource": "/{proxy+}",
+        "path": "/secret",
+        "httpMethod": "POST",
+        "body": content,
+    }
+
+    # act
+    result = handler.handler(event, None)
+
+    # assert
+    assert result.get("statusCode") == 200
+    assert result.get("body").get("SecretId")
+
+    secret_id = result.get("body").get("SecretId")
+    response = table.get_item(Key={"SecretId": secret_id})
+    assert "Item" in response
+
+    item = response["Item"]
+    assert item.get("SecretContent") == content
+
+
+def test_boto3_error_occurs_on_create_secret(mocker):
+
+    # arranje
+    region = "us-east-1"
+    os.environ[handler.ENV_DYNAMO_DB_REGION] = region
+
+    event = {
+        "resource": "/{proxy+}",
+        "path": "/secret",
+        "httpMethod": "POST",
+    }
+
+    mock = mocker.patch("handler._get_dynamodb_client")
+    mock.side_effect = ClientError({}, "SomeOperation")
+
+    # act
+    result = handler.handler(event, None)
+
+    # assert
+    assert result.get("statusCode") == 500
